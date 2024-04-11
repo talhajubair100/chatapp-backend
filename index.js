e7142bcf26bf0702ae0ff6b41c50d39d55e1d741
talhajubair100 <talhajubair100.bd@gmail.com>
2024-04-11 18:04:51 +0600

    Update CORS configuration to allow additional methods and headers

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ const corsOptions = {
     }
     return callback(null, true);
   },
-  credentials: true // Allow credentials to be sent with the request
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range']
+
 };
 
 
